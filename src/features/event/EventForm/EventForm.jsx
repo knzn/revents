ba27db55bc4b80bfa28b09bbdaebb76c7ce2a6a7
knzn <rdacles08@gmail.com>
2018-07-09 15:12:08 +0800

    End of  section 5

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,20 +1,42 @@
 import React, { Component } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 
+const emptyEvent = {
+  title: "",
+  date: "",
+  city: "",
+  venue: "",
+  hostedBy: ""
+};
+
 class EventForm extends Component {
   state = {
-    event: {
-      title: "",
-      date: "",
-      city: "",
-      venue: "",
-      hostedBy: ""
-    }
+    event: emptyEvent
   };
+
+  componentDidMount() {
+    if (this.props.selectedEvent !== null) {
+      this.setState({
+        event: this.props.selectedEvent
+      });
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.selectedEvent !== this.props.selectedEvent) {
+      this.setState({
+        event: nextProps.selectedEvent || emptyEvent
+      });
+    }
+  }
 
   onFormSubmit = evt => {
     evt.preventDefault();
-    this.props.createEvent(this.state.event);
+    if (this.state.event.id) {
+      this.props.updateEvent(this.state.event);
+    } else {
+      this.props.createEvent(this.state.event);
+    }
   };
 
   onInputChange = evt => {
@@ -48,7 +70,7 @@ class EventForm extends Component {
               onChange={this.onInputChange}
               value={event.date}
               type="date"
-              placeholder="mm/dd/yy"
+              placeholder="dd/mm/yy"
             />
           </Form.Field>
           <Form.Field>
@@ -74,7 +96,7 @@ class EventForm extends Component {
             <input
               name="hostedBy"
               onChange={this.onInputChange}
-              value={event.HostedBy}
+              value={event.hostedBy}
               placeholder="Enter the name of person hosting"
             />
           </Form.Field>
